a5ab9a92b4e0f99097de7d39d8ad995b117d1b66
luozhi1024 <13101606873@163.com>
2018-08-13 23:53:04 +0800

    appvue

---
+++ appvue/src/router.js
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import movie from '@/views/movie/movie.vue'
+import music from '@/views/music/music.vue'
+import book from '@/views/book/book.vue'
+import photo from '@/views/photo/photo.vue'
+
+Vue.use(Router);
+
+export default new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [{
+        path: '/',
+        redirect: '/movie'
+    },
+        {
+            path: '/movie',
+            name: 'movie',
+            component: movie
+        }, {
+            path: '/music',
+            name: 'music',
+            component: music
+        }, {
+            path: '/book',
+            name: 'book',
+            component: book
+        }, {
+            path: '/photo',
+            name: 'photo',
+            component: photo
+        }
+    ],
+});
