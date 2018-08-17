4955cc1dd8d6b1af279078e0870e74fd6c8aa296
luozhi1024 <13101606873@163.com>
2018-08-17 14:48:11 +0800

    appvue

--- appvue/src/views/book/book.vue
+++ appvue/src/views/book/book.vue
@@ -1,15 +1,43 @@
 <template>
-    <h1>book</h1>
+  <swipe class="my-swipe">
+      <swipe class="slide1"></swipe>
+      <swipe class="slide2"></swipe>
+      <swipe class="slide3"></swipe>
+  </swipe>
 </template>
 
 
 <script>
+    import "vue-swipe/dist/vue-swipe.css";
+    import  {Swipe} from "vue-swipe";
     export default {
 created(){
             this.$emit("switchtab","book")
+        },
+        components:{
+            swipe : Swipe,
+
         }
     }
 </script>
 
 <style lang="scss" scoped>
+    .my-swipe {
+        height: 200px;
+        color: #fff;
+        font-size: 30px;
+        text-align: center;
+    }
+    .slide1 {
+        background-color: #0089dc;
+        color: #fff;
+    }
+    .slide2 {
+        background-color: #ffd705;
+        color: #000;
+    }
+    .slide3 {
+        background-color: #ff2d4b;
+        color: #fff;
+    }
 </style>
